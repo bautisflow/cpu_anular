753607df6e9d8c0930dfc09595bc478d5781912e
Jaime Bautista <jaimebs94@gmail.com>
2020-05-08 23:07:56 +0200

    PC transfer commit

--- codigo.pl
+++ codigo.pl
@@ -160,14 +160,19 @@ execute_instruction(Ea,swap(I,J),Es) :-
     subcopy_term(N,Ea,Es).
 
 execute_instruction(Ea,move(I),Es) :-
-
+    var(I),
+    functor(Ea,regs,N),
+    execute_instruction(Ea,move(1),N,Es).
+execute_instruction(Ea,move(I),_,Es) :- % Backtraking
+    nonvar(I),
+    execute_instruction(Ea,move(I),Es).
 execute_instruction(Ea,move(I),Max,Es) :- % Backtraking
     I =< Max,
-    execute_instruction(Ea,move(I),Es), !,
     I1 is I+1,
+    execute_instruction(Ea,move(I),Es),
     execute_instruction(Ea,move(I1),Max,Es).
 
-% Aqui hay que pasarle functor delh mismo tamaño
+% Aqui hay que pasarle functor del mismo tamaño
 subcopy_term(0,_,_).
 subcopy_term(I,Ea,Es) :-
     arg(I,Es,X1),
@@ -194,78 +199,6 @@ code_generator(Ei,Ef,[L|Ll]) :-
 
 %%%% Auxiliary predicates
 
-
-%% ----------------------------------------%%
-% -- Predicates for Peano representation -- %
-%% ----------------------------------------%%
-
-:- prop nat(X) #"@var{X} es un n@'{u}mero natural en notaci@'{o}n de Peano. @includedef{nat/1}".
-nat(0).
-nat(s(N)) :-
-  nat(N).
-
-:- prop nat_eq(X,Y) # "@var{X} y @var{Y} son iguales la una a la otra. @includedef{nat_eq/2}".
-nat_eq(0,0).
-nat_eq(s(X),s(Y)) :-
-    nat_eq(X,Y).
-
-:- prop nat_gt(X,Y) # "@var{X} es mayor estricto (>) que @var{Y}. @includedef{nat_gt/2}".
-nat_gt(s(X),0) :-
-    nat(X).
-nat_gt(s(X),s(Y)) :-
-    nat_gt(X,Y).
-
-:- prop nat_geq(X,Y) # "@var{X} es mayor o igual (>=) que @var{Y}. @includedef{nat_geq/2}".
-nat_geq(0,0).
-nat_geq(s(X),0) :-
-    nat(X).
-nat_geq(s(X),s(Y)) :-
-    nat_geq(X,Y).
-
-:- prop nat_even(X) # "@var{X} es par. @includedef{nat_even/1}".
-nat_even(0).
-nat_even(s(s(X))) :-
-    nat_even(X).
-
-:- prop nat_odd(X) # "@var{X} es impar. @includedef{nat_odd/1}".
-nat_odd(s(0)).
-nat_odd(s(s(X))) :-
-    nat_odd(X).
-
-:- pred nat_add(X,Y,S) # "@var{S} es el resultado de la suma @var{X} + @var{Y}. @includedef{nat_add/3}".
-nat_add(0,X,X).
-nat_add(s(X),Y,s(S)) :-
-    nat_add(X,Y,S).
-
-:- pred nat_prod(X,Y,P) # "@var{P} es el producto de multiplicar @var{X} * @var{Y}. @includedef{nat_prod/3}".
-nat_prod(0,_,0).
-nat_prod(s(X),Y,P) :-
-    nat_add(Y,Z,P),
-    nat_prod(X,Y,Z).
-
-:- pred nat_mod(X,Y,R) # "@var{R} es el resultado de la operaci@'{o}n m@'{o}dulo, es decir, el resto de la divisi@'{o}n @var{X} / @var{Y}. @includedef{nat_mod/3}".
-nat_mod(X,Y,X) :-
-    nat_gt(Y,X).
-nat_mod(X,Y,R) :-
-    nat_add(Z,Y,X),
-    nat_mod(Z,Y,R).
-
-:- pred nat_round(X,R,Y) # "Redondea el n@'{u}mero dado por @var{X} en funci@'{o}n del resto, @var{R}, obtenido en una divisi@'{o}n anterior al n@'{u}mero entero m@'{a}s cercano, devolviendo el resultado del redondeo en @var{Y}. Para la toma de decisiones, especialmente en la mitad de dos n@'{u}meros, se toma como referencia el est@'{a}ndar IEEE 754 de 2008. @includedef{nat_round/3}".
-nat_round(X,R,s(X)) :-
-    nat_prod(R,s(s(0)),Y),
-    nat_gt(Y,X).
-nat_round(X,R,X) :-
-    nat_prod(R,s(s(0)),Y),
-    nat_gt(X,Y).
-nat_round(X,R,s(X)) :-
-    nat_prod(R,s(s(0)),Y),
-    nat_eq(Y,X),
-    nat_odd(X).
-nat_round(X,R,X) :-
-    nat_prod(R,s(s(0)),Y),
-    nat_eq(Y,X),
-    nat_even(X).
-
 %% ---------------------------------------%%
 % -- Predicates for List representation -- %
 %% ---------------------------------------%%
@@ -275,40 +208,11 @@ list([]).
 list([_|Xs]) :-
      list(Xs).
 
-:- prop list_members_nat(X) # "@var{X} es una lista compuesta por n@'{u}meros naturales en notaci@'{o}n de Peano. @includedef{list_members_nat/1}".
-list_members_nat([X]) :-
-    nat(X).
-list_members_nat([X|Xs]) :-
-    nat(X),
-    list_members_nat(Xs).
-
-:- prop sublist_members_nat(X) # "@var{X} es una lista compuesta por listas donde sus elementos son n@'{u}meros naturales en notaci@'{o}n de Peano. @includedef{sublist_members_nat/1}".
-sublist_members_nat([]).
-sublist_members_nat([X|Xs]) :-
-    list_members_nat(X),
-    sublist_members_nat(Xs).
-
-% Seguro que se puede hacer mas bonito y recursivo
-:- prop list_same_length(Xs,Ys) # "Comprueba si la lista @var{Xs} tiene la misma longitud que @var{Ys}. @includedef{list_same_length/2}".
-list_same_length(Xs,Ys) :-
-    list_length(Xs, Zs1),
-    list_length(Ys, Zs2),
-    nat_eq(Zs1,Zs2).
-
-:- pred list_select(X,N,Y) # "Devuelve en @var{Y} el @var{N}-@'{e}simo n@'{u}mero de la lista @var{X}. @includedef{list_select/3}".
-list_select([X|_],s(0),X).
-list_select([_|Xs],s(N),Y) :-
-    list_select(Xs,N,Y).
-
 :- pred list_append(Xs,Ys,Zs) # "@var{Zs} ser@'{a} el resultado de introducir la lista @var{Ys} al final de la lista @var{Xs}. @includedef{list_append/3}".
 list_append([],Ys,Ys).
 list_append([X|Xs],Ys,[X|Zs]) :-
 	list_append(Xs,Ys,Zs).
 
-:- pred list_length(Xs,L) # "Calcula la longitud de una lista @var{Xs}, devolvi@'{e}ndolo en @var{L}. @includedef{list_length/2}".
-list_length([],0).
-list_length([_|Xs],s(L)) :-
-	list_length(Xs,L).
 
 :- pred list_reverse(Xs,Ys) # "@var{Ys} ser@'{a} la lista @var{Xs} del rev@'{e}s, es decir, intercambiando cada elemento 'n' de @var{Xs} por longitud(@var{Xs}) -'n' - 1. @includedef{list_reverse/2}".
 list_reverse(Xs,Ys) :-
@@ -320,9 +224,8 @@ list_reverse([X|Xs],Acc,Ys) :-
 
 :- pred list_flatten(Xs,Ys) # "Aplana la lista @var{Xs}, devolviendo el resultado en @var{Ys}. El aplanado consiste en generar una lista de elementos, en este caso naturales, a partir de una lista cuyos elementos son listas. @includedef{list_flatten/2}".
 list_flatten([],[]).
+list_flatten(X,[X]).
 list_flatten([X|Xs],Y) :-
     list_flatten(X,Ys1),
     list_flatten(Xs,Ys2),
     list_append(Ys1,Ys2,Y).
-list_flatten(X,[X]) :-
-    nat(X).
